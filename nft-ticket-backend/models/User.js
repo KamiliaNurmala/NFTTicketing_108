1cf2c28ade2d4973dd14d62230e67396d1d4f823
KamiliaNurmala <nurmalakamilia@gmail.com>
2026-01-04 14:56:37 +0700

    create User model with password hashing

---
+++ nft-ticket-backend/models/User.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+const bcrypt = require('bcrypt');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  walletAddress: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+}, {
+  tableName: 'users',
+  timestamps: true,
+  hooks: {
+    beforeCreate: async (user) => {
+      user.password = await bcrypt.hash(user.password, 10);
+    }
+  }
+});
+
+User.prototype.validatePassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+module.exports = User;
